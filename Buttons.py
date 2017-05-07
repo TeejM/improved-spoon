64822348d1f946310c5b8c2f51cba6bf383d7bfa
TeejM <teejmathews@gmail.com>
2017-05-06 21:21:21 -0500

    Add files via upload

---
+++ Buttons.py
@@ -0,0 +1,27 @@
+from Tkinter import *
+
+window = Tk()
+
+LeftFrame = Frame(window)
+LeftFrame.pack(side=LEFT)
+RightFrame = Frame(window)
+RightFrame.pack(side=RIGHT)
+
+
+def toggle(i):
+    if Buttons[i].config("text")[-1] == "High":
+        Buttons[i].config(text="Low")
+    else:
+        Buttons[i].config(text="High")
+
+
+Buttons = {i: Button(LeftFrame, bg="white", text="High", height=3, width=5, command=lambda i=i: toggle(i)) for i in range (1, 9)}
+for i in range(1, len(Buttons)+1):
+    Buttons[i].pack(anchor=W)
+
+# print Buttons[1]
+img = PhotoImage(file="map.gif")
+Image = Label(RightFrame, image=img)
+Image.pack(anchor=E)
+
+window.mainloop()
