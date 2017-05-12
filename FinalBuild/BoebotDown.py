782096cc3422ff994a13817f613ceb39c0816bf6
TeejM <teejmathews@gmail.com>
2017-05-11 22:25:04 -0500

    Add files via upload

--- FinalBuild/BoebotDown.py
+++ FinalBuild/BoebotDown.py
@@ -1,8 +1,10 @@
 from Tkinter import *
-#import RPi.GPIO as GPIO
+# import RPi.GPIO as GPIO
 from random import randint
 import serial
 
+# GPIO.setmode(GPIO.BCM)
+# GPIO.setwarnings(False)
 HEIGHT = 440
 WIDTH = 660
 
@@ -11,21 +13,28 @@ class Game(Frame):
     def __init__(self, master):
         Frame.__init__(self, master)
         self.master = master
-        # Binds the key enter to the quit() method
+        # Binds the key Escape to the quit() method
         self.master.bind("<Escape>", self.quit)
+        # Binds the key Enter to the process() method
         self.master.bind("<Return>", self.process)
+        # Sets the window height and width
         self.master.minsize(width=WIDTH, height=HEIGHT)
         self.master.maxsize(width=WIDTH, height=HEIGHT)
-        self.text = Label(self.master, text="Congratulations on being selected for the job! \n \
-Our Boebot has broken down and you must repair it! \n \
-Your task is to complete the following logic circuits by turning the available inputs on or off.")
+        # Initial text label and button to display
+        self.text = Label(self.master, text="Congratulations on being selected for the job! \n"
+                                            + "Our Boebot has broken down and you must repair it! \n"
+                                            + "Your task is to complete the following logic circuits by turning the available inputs on or off. \n"
+                                            + "After you make your selections, press 'ENTER' to check your answer and proceed to the next puzzle",
+                          font=("Comic Sans MS", 16), wraplength=WIDTH - 20)
         self.text.pack(side=TOP)
-        self.begin = Button(self.master, text="Begin", command=lambda: self.setupGUI(8,1,1))
+        # Binds the setupGUI method to the button
+        self.begin = Button(self.master, text="Begin", font=("Comic Sans MS", 16), padx=20, bd=5, command=lambda: self.setupGUI(8,1,1))
         self.begin.pack(side=BOTTOM)
         self.puzzle = "Puzzle1.gif"
     # Command to prepare screen for each puzzle
     def setupGUI(self, BUTTONS, COLUMNS, ROWS):
-    # set up buttons
+    # Setup buttons        
+        # Removes the initial label and button
         self.text.pack_forget()
         self.begin.pack_forget()
         # Generate a PhotoObject; default is off
@@ -53,7 +62,7 @@ Your task is to complete the following logic circuits by turning the available i
             # Make Buttons rowspan larger if multiple Labels
             if (ROWS > BUTTONS): Buttons[i].grid(rowspan=ROWS/BUTTONS+1)
 
-    # set up labels
+    # Setup labels
         columns = [str(c) for c in range(COLUMNS)]
         rows = [str(r) for r in range(ROWS)]
         # Generate and create key for each PhotoObject
@@ -81,35 +90,64 @@ Your task is to complete the following logic circuits by turning the available i
             label[k].grid_propagate(False)
             # Make Labels rowspan larger if multiple Buttons
             if (BUTTONS > ROWS): label[k].grid(rowspan=BUTTONS/ROWS)
+    # Method to be called when enter is pressed
     def process(self, event):
-        if (self.puzzle == "Puzzle1.gif" and setter[0] == 1 and setter[1] == 0 and setter[2] == 1 and setter[3] == 0 and setter[4] == 1 and setter[5] == 0 and setter[6] == 1 and setter[7] == 1):
+        # Checks for the current puzzle and the currently entered solution and changes puzzles if it's correct
+        if (self.puzzle == "Puzzle1.gif" and setter[0] == 1 and setter[1] == 0 and setter[2] == 1 and setter[3] == 0 and \
+                            setter[4] == 1 and setter[5] == 0 and setter[6] == 1 and setter[7] == 1):
             self.puzzle = "Puzzle2.gif"
             frame.setupGUI(8,1,1)
-        elif (self.puzzle == "Puzzle2.gif" and setter[0] == 1 and setter[1] == 0 and setter[2] == 1 and setter[3] == 1 and setter[4] == 0 and setter[5] == 1 and setter[6] == 1 and setter[7] == 0):
+        elif (self.puzzle == "Puzzle2.gif" and setter[0] == 1 and setter[1] == 0 and setter[2] == 1 and setter[3] == 1 and \
+                            setter[4] == 0 and setter[5] == 1 and setter[6] == 1 and setter[7] == 0):
             self.puzzle = "Puzzle3.gif"
             frame.setupGUI(8,1,1)
-        elif (self.puzzle == "Puzzle3.gif" and setter[0] == 0 and setter[1] == 1 and setter[2] == 1 and setter[3] == 1 and setter[4] == 0 and setter[5] == 1 and setter[6] == 1 and setter[7] == 1):
+        elif (self.puzzle == "Puzzle3.gif" and setter[0] == 0 and setter[1] == 1 and setter[2] == 1 and setter[3] == 1 and \
+                            setter[4] == 0 and setter[5] == 1 and setter[6] == 1 and setter[7] == 1):
             self.puzzle = "Puzzle4.gif"
             frame.setupGUI(8,1,1)
-        elif (self.puzzle == "Puzzle4.gif" and setter[0] == 1 and setter[1] == 0 and setter[2] == 1 and setter[3] == 1 and setter[4] == 0 and setter[5] == 1 and setter[6] == 0 and setter[7] == 0):
+        elif (self.puzzle == "Puzzle4.gif" and setter[0] == 1 and setter[1] == 0 and setter[2] == 1 and setter[3] == 1 and \
+                              setter[4] == 0 and setter[5] == 1 and setter[6] == 0 and setter[7] == 0):
             self.puzzle = "Win.gif"
             frame.setupGUI(8,1,1)
             self.genRandom()
+    # Generates a random decimal number and displays it in binary using LEDs
     def genRandom(self):
-        num = randint(0,5)
-        
-##        gpio = [26,19,13]
-##        for i in gpio:
-##            GPIO.setup(i, GPIO.OUT)
-##        GPIO.output([gpio], HIGH)
-##        num = []
-##        for i in range(0,3):
-##            num.append(randint(0,1))
-##        for i in range(len(num)):
-##            if (num[i] == 1):
-##                GPIO.output(gpio[i], GPIO.HIGH)
-##            else:
-##                GPIO.output(gpio[i], GPIO.LOW) 
+        # GPIO pins to use
+        gpio = [18, 23, 24]
+        # Sets the pins as output
+        for i in gpio:
+            GPIO.setup(i, GPIO.OUT)
+        # Turns all pins on initially
+        GPIO.output(gpio[0], 1)
+        GPIO.output(gpio[1], 1)
+        GPIO.output(gpio[2], 1)
+        # Generates a random integer from 0 to 5 inclusive
+        num = randint(0, 5)
+        # Turns certain LEDs on depending on the number generated
+        if (num == 0):
+            GPIO.output(gpio[0], 0)
+            GPIO.output(gpio[1], 0)
+            GPIO.output(gpio[2], 0)
+        elif (num == 1):
+            GPIO.output(gpio[0], 0)
+            GPIO.output(gpio[1], 0)
+            GPIO.output(gpio[2], 1)
+        elif (num == 2):
+            GPIO.output(gpio[0], 0)
+            GPIO.output(gpio[1], 1)
+            GPIO.output(gpio[2], 0)
+        elif (num == 3):
+            GPIO.output(gpio[0], 0)
+            GPIO.output(gpio[1], 1)
+            GPIO.output(gpio[2], 1)
+        elif (num == 4):
+            GPIO.output(gpio[0], 1)
+            GPIO.output(gpio[1], 0)
+            GPIO.output(gpio[2], 0)
+        elif (num == 5):
+            GPIO.output(gpio[0], 1)
+            GPIO.output(gpio[1], 0)
+            GPIO.output(gpio[2], 1)
     # Quits the program, currently configured to Escape in the constructor
     def quit(self, event):
         self.master.destroy()
@@ -132,12 +170,10 @@ def toggle(i, bphoto, setter, Buttons):
 
 
 ###########################################################################
-
-
 # Run Tkinter
 window = Tk()
-# Figure out screen dimensions
-w, h = window.winfo_screenwidth(), window.winfo_screenheight()
+# Window title
+window.title("Boebot Down!")
 # Make window full screen
 # window.overrideredirect(1)
 frame = Game(window)
