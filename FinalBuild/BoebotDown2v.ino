21df26de5b79407cef4cbf0fc090c1c719ab2672
Benjamin Hargrove <blh037@latech.edu>
2017-05-15 20:38:52 -0500

    Add files via upload

---
+++ FinalBuild/BoebotDown2v.ino
@@ -0,0 +1,25 @@
+ #include <Servo.h>
+ Servo  leftservo;
+ Servo  rightservo;
+
+void setup() {
+ pinMode(2, OUTPUT);
+ rightservo.attach(2);
+ pinMode(3, OUTPUT);
+ leftservo.attach(3);
+}
+
+void loop() {
+ if (analogRead(4) >= 1000) {rotate();}
+ else if (analogRead(1) >= 1000) {still(); delay(1000);}
+}
+
+void rotate() {
+                rightservo.writeMicroseconds(1670);
+                leftservo.writeMicroseconds(1450);
+               }
+void still() {
+                rightservo.writeMicroseconds(1500);
+                leftservo.writeMicroseconds(1500);
+              }
+
